829a3d0a668e0ac7497297ddc3cdb7d720258973
James Scott <James_Scott@sweetwater.com>
2024-03-27 09:30:57 -0400

    Update tool4d

--- Command4D.sh
+++ Command4D.sh
@@ -6,11 +6,12 @@
 # 
 
 
-PROJECT=/Users/jscott/Documents/gProjects/4DExplorer/Project/Contacts.4DProject
-# PROJECT=/Users/jamesscott/iDocuments/gProjects/4DExplorer/Project/Contacts.4DProject
+# PROJECT=/Users/jscott/Documents/gProjects/4DExplorer/Project/Contacts.4DProject
+PROJECT="/Users/jamesscott/iDocuments/gProjects/4DExplorer/Project/Contacts.4DProject"
 FOURD=/Volumes/Development4D/4Dv20.2/4D.app
 # FOURD=/Applications/4Dv20.2/4D.app
-TOOL4D=/Volumes/Development4D/tool4d.app/Contents/MacOS/tool4d
+# TOOL4D=/Volumes/Development4D/tool4d.app/Contents/MacOS/tool4d
+TOOL4D=/Applications/tool4d.app/Contents/MacOS/tool4d
 
 ACTION=${1:-checksyntax}
 
@@ -18,8 +19,8 @@ rm fourDout.log
 
 echo "Action requested: " $ACTION
 
-open --stdout=fourDout.log -a $FOURD --args --project $PROJECT --user-param $ACTION --create-data
-# $TOOL4D --project=$PROJECT --user-param=$ACTION
+# open --stdout=fourDout.log -a $FOURD --args --project $PROJECT --user-param $ACTION --create-data
+$TOOL4D --project=$PROJECT --opening-mode="interpreted" --user-param=$ACTION --dataless
 
 echo fourDout.log
 
