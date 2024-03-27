f2d754f283a8a13ae150847809f8c37b743bf90a
James Scott Jr <skoona@gmail.com>
2024-03-27 16:08:38 -0400

    Update the compile workflow

--- Command4D.sh
+++ Command4D.sh
@@ -6,16 +6,16 @@
 # 
 
 
-# PROJECT=/Users/jscott/Documents/gProjects/4DExplorer/Project/Contacts.4DProject
-PROJECT="/Users/jamesscott/iDocuments/gProjects/4DExplorer/Project/Contacts.4DProject"
+PROJECT=/Users/jscott/Documents/gProjects/4DExplorer/Project/Contacts.4DProject
+# PROJECT="/Users/jamesscott/iDocuments/gProjects/4DExplorer/Project/Contacts.4DProject"
 
-# FOURD=/Volumes/Development4D/4Dv20.2/4D.app
+FOURD=/Volumes/Development4D/4Dv20.2/4D.app
 # FOURD=/Applications/4Dv20.2/4D.app
-FOURD=/Applications/4Dv20.2/4D.app/Contents/MacOS/4D
+# FOURD=/Applications/4Dv20.2/4D.app/Contents/MacOS/4D
 FOURDCLIENT=/Applications/4Dv20.2/4D.app/Contents/MacOS/4D
 
-# TOOL4D=/Volumes/Development4D/tool4d.app/Contents/MacOS/tool4d
-TOOL4D=/Applications/tool4d.app/Contents/MacOS/tool4d
+TOOL4D=/Volumes/Development4D/tool4d.app/Contents/MacOS/tool4d
+# TOOL4D=/Applications/tool4d.app/Contents/MacOS/tool4d
 
 ACTION=${1:-checksyntax}
 
@@ -25,7 +25,7 @@ echo "Action requested: " $ACTION
 
 # open --stdout=fourDout.log -a $FOURD --args --project $PROJECT --user-param $ACTION --create-data
 # $FOURD --project=$PROJECT --user-param=$ACTION --create-data --headless
-$TOOL4D  --project=$PROJECT --user-param=$ACTION --skip-onstartup  --dataless --startup-method=GH_SyntaxCheck_P 
+$TOOL4D  --opening-mode=interpreted --project=$PROJECT --user-param=$ACTION --skip-onstartup  --dataless --startup-method=GH_SyntaxCheck_P 
 # $FOURDCLIENT --project=$PROJECT --user-param=$ACTION --dataless --headless
 
 echo fourDout.log
