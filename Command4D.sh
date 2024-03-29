05701628589f9c07ae60aa572c60eb25153dc0d6
James Scott Jr <skoona@gmail.com>
2024-03-29 14:48:32 -0400

    Updated my version of local pipeline test script

--- Command4D.sh
+++ Command4D.sh
@@ -12,7 +12,8 @@ PROJECT=/Users/jscott/Documents/gProjects/4DExplorer/Project/Contacts.4DProject
 FOURD=/Volumes/Development4D/4Dv20.2/4D.app
 # FOURD=/Applications/4Dv20.2/4D.app
 # FOURD=/Applications/4Dv20.2/4D.app/Contents/MacOS/4D
-FOURDCLIENT=/Applications/4Dv20.2/4D.app/Contents/MacOS/4D
+# FOURDCLIENT=/Applications/4Dv20.2/4D.app/Contents/MacOS/4D
+FOURDCLIENT=/Volumes/Development4D/4Dv20.2/4D.app/Contents/MacOS/4D
 
 TOOL4D=/Volumes/Development4D/4Dv20R3/tool4d.app/Contents/MacOS/tool4d
 # TOOL4D=/Applications/tool4d.app/Contents/MacOS/tool4d
@@ -27,8 +28,8 @@ echo "Action requested: " $ACTION
 
 # open --stdout=fourDout.log -a $FOURD --args --project $PROJECT -user-param $ACTION --dataless --skip-onstartup --startup-method=GHPipeline_EntryPoint_P 
 # $FOURD --project=$PROJECT --user-param=$ACTION --create-data --headless --skip-onstartup --dataless --startup-method=GHPipeline_EntryPoint_P 
-$TOOL4D  --opening-mode=interpreted --project=$PROJECT --user-param=$ACTION --skip-onstartup --dataless --startup-method=GHPipeline_EntryPoint_P 
-# $FOURDCLIENT --project=$PROJECT --user-param=$ACTION --dataless --headless --skip-onstartup --dataless --startup-method=GHPipeline_EntryPoint_P 
+# $TOOL4D  --opening-mode=interpreted --project=$PROJECT --user-param=$ACTION --skip-onstartup --dataless --startup-method=GHPipeline_EntryPoint_P 
+$FOURDCLIENT --project=$PROJECT --user-param=$ACTION --dataless --headless --skip-onstartup --dataless --startup-method=GHPipeline_EntryPoint_P 
 
 [[ -f fourDout.log ]] && {
     echo fourDout.log
